48f3e54acd44fd9622637e04c8f578314a78b3b7
prajitrr <151738064+prajitrr@users.noreply.github.com>
2023-12-03 18:47:16 -0800

    Modules to retrieve structures/SMILES and MS data
    Added a module to retrieve images of chemical structures and associated SMILES IDs, and a module to automate the retrieval of result files of job queries from the Proteomics2 and GNPS2 databases.

---
+++ queryretriever.py
@@ -0,0 +1,63 @@
+#Receives a link to either a Proteomics2 or GNPS2 job query and downloads the
+#the results file of the job as a tsv
+#Specify a file label to uniquely label each file
+#Contact Prajit Rajkumar (prajkumar@ucsd.edu) for any questions
+
+#Note that you may have to install some of the imports using 
+#pip or another installer
+import time
+import ast
+import numpy as np
+import pandas as pd
+import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+def prot2_retrieve(url, file_label, wait_time=10):
+    """
+    Retrieve the result file from a Proteomics2 job query in tsv format.
+
+    :param str url: URL to a Proteomics2 job query
+    :param str file_label: A label used to uniquely identify the file
+    :param int wait_time: Time to wait to allow the result file to load 
+                          in; change as necessary based on result file 
+                          sizes, defaults to 10
+    :return: The number of scans in the result file; returns 0 for empty files
+    :rtype: int
+    """
+    load_url = requests.get(url, allow_redirects=True)
+    with urlopen(url):
+        time.sleep(wait_time)
+    task_ID = url[url.find("task=") + 5:]
+    processed_url = ("https://proteomics2.ucsd.edu/ProteoSAFe/" 
+                     + "QueryResult?task=" + task_ID + "&file=extract_results"
+                     + "-main_dynamic_extracted.db&pageSize=" 
+                     + "-1&offset=0&query=&_=")
+    r = requests.get(processed_url, allow_redirects=True)
+    page = r.content
+    soup = BeautifulSoup(page, 'html.parser')
+    text = soup.find_all(string=True)
+    text = "".join(text)
+    if (text[0:35] == "Apache Tomcat/6.0.24 - Error report"):
+        return 0
+    text = text[text.find("row_data") + 10:-1]
+    text = text.replace("\n", "")
+    text = ast.literal_eval(text)
+    text = pd.DataFrame(text)
+    text.to_csv("prot2_" + "queried_file" + file_label + ".tsv", sep="\t")
+    return len(text)
+
+def gnps_retrieve(url, file_label):
+    """
+    Retrieve the result file from a GNPS2 job query in tsv format.
+
+    :param str url: URL to a GNPS2 job query
+    :param str file_label: A label used to uniquely identify the file
+    """
+    task_ID = url[url.find("task=") + 5:]
+    processed_url = ("https://gnps2.org/resultfile?task=" + task_ID 
+                     + "&file=nf_output/msql/merged_query_results.tsv")
+    r = requests.get(processed_url, allow_redirects=True)
+    file_name = "gnps_" + "queried_file" + file_label + ".tsv"
+    open(file_name, "wb").write(r.content)
---
+++ structureretriever.py
@@ -0,0 +1,39 @@
+#Receives a list of molecules and outputs a datframe with SMILES and links
+#to structure drawings of the molecules
+#Note that you may have to expand some names in the input to their long form 
+#(eg GABA -> Gamma aminobutyric acid)
+#Contact Prajit Rajkumar (prajkumar@ucsd.edu) for any questions
+
+#Note that you may have to install some of the imports using 
+#pip or another installer
+import numpy as np
+import pandas as pd
+from urllib.request import urlopen
+from urllib.parse import quote
+
+def retriever(list_of_names):
+    """
+    Retrieve the SMILES ID and images of a specified list of molecules.
+
+    :param list list_of_names: List of n molecules, preferably using IUPAC 
+                               naming conventions
+    :return: An n by 3 dataframe containing the SMILES IDs and images 
+             of each molecule
+    :rtype: pandas.core.frame.DataFrame
+    """
+    structures_df = pd.DataFrame(columns=["Molecule","SMILES", "Image"])
+    i = 0
+    for item in list_of_names:
+        formatted_item = item.replace(" ","%20")
+        try:
+            url = ('http://cactus.nci.nih.gov/chemical/structure/' 
+                   + formatted_item + '/smiles')
+            smiles_id = urlopen(url).read().decode('utf8')
+            image_url = ('https://cactus.nci.nih.gov/chemical/structure/' 
+                         + smiles_id + '/image')
+        except:
+            smiles_id = 'Unavailable'
+            image_url = 'Unavailable'
+        structures_df.loc[i] = [item, smiles_id, image_url]
+        i = i + 1
+    return structures_df
